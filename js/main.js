433e9359a128b863d938cfa31d5fb11a37e2cacd
joseelizondo22 <jose22je@outlook.com>
2019-01-21 19:22:53 -0600

    Updates

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial State of Menu
+let ShowMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!ShowMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        
+        // Set Menu State
+        ShowMenu = true; 
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // set Menu state
+        ShowMenu = false;
+    }
+}
